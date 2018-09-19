2ce1be1bbaa9e32e9fb4aac3836e3f32f18bdecf
Jae Choi <j.thechois@gmail.com>
2018-09-19 15:08:26 +0900

    complete pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,9 +1,28 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+  this.storage = {}
+  this.id = 0
+}
 
-if ( typeof module === "object" && typeof module.exports === "object" ) {
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.id] = value
+  this.id++
+}
+Queue.prototype.dequeue = function () {
+  if (this.id > 0) {
+    var minIdx = Math.min.apply(null, Object.keys(this.storage))
+    var returnVal = this.storage[minIdx]
+    delete this.storage[minIdx]
+    return returnVal
+  };
+}
+Queue.prototype.size = function () {
+  var lengthQueue = Object.keys(this.storage).length
+  return lengthQueue
+}
+
+if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
     Queue
   }
