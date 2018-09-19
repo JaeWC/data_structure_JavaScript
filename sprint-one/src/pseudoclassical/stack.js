71d945cce33eca3d6d9e8d9d1b6bcad8f5bb9f37
Jae Choi <j.thechois@gmail.com>
2018-09-19 15:07:37 +0900

    complete pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,9 +1,26 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+  this.storage = {}
+  this.count = 0
+}
 
-if ( typeof module === "object" && typeof module.exports === "object" ) {
+Stack.prototype.push = function (value) {
+  this.count++
+  this.storage[this.count] = value
+}
+Stack.prototype.pop = function () {
+  if (this.count > 0) {
+    var returnVal = this.storage[this.count]
+    this.count--
+    return returnVal
+  }
+}
+Stack.prototype.size = function () {
+  if (this.count >= 0) return this.count
+}
+
+if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
     Stack
   }
