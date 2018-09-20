178dad871f1fe4100344474d3899fb35ec773788
Jae Choi <j.thechois@gmail.com>
2018-09-20 11:38:11 +0900

    draft of linkedList function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,32 +1,50 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
+var LinkedList = function () {
+  var list = {}
+  list.head = null
+  list.tail = null
 
-  list.addToTail = function(value) {
-  };
+  // takes a value and adds it to the end of the list
+  list.addToTail = function (value) {
+    if (this.head === null) {
+      this.head = new Node(value)
+      this.tail = this.head
+    } else {
+      this.tail = new Node(value)
+    }
+  }
 
-  list.removeHead = function() {
-  };
+  // removes the first node from the list and returns its value
+  list.removeHead = function () {
+    var removedVal = list.head.value
+    delete list.head
+    this.head = list.tail
+    return removedVal
+  }
 
-  list.contains = function(target) {
-  };
+  // returns boolean reflecting whether or not the passsed-in value is in the linked list
+  list.contains = function (target) {
+    for (var prop in this) {
+      if (this[prop].value === target) {
+        return true
+      }
+    }
+    return false
+  }
+  return list
+}
 
-  return list;
-};
+var Node = function (value) {
+  var node = {}
 
-var Node = function(value) {
-  var node = {};
+  node.value = value
+  node.next = null
 
-  node.value = value;
-  node.next = null;
-
-  return node;
-};
+  return node
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-if ( typeof module === "object" && typeof module.exports === "object" ) {
-  module.exports = LinkedList;
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = LinkedList
 }
