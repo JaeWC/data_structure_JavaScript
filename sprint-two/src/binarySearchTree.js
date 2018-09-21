a31a541823f3a303af4c255327cba278b3faf06a
Jae Choi <j.thechois@gmail.com>
2018-09-21 20:16:35 +0900

    complete binarySearchTree function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,11 +1,74 @@
-var BinarySearchTree = function(value) {
-};
+var BinarySearchTree = function (value) {
+  var newBinarySearchTree = Object.create(binaryMethods)
+  newBinarySearchTree.value = value
+  newBinarySearchTree.left = null
+  newBinarySearchTree.right = null
+  return newBinarySearchTree
+}
 
+var binaryMethods = {}
 
+// accepts a value and places in the tree in the correct position
+binaryMethods.insert = function (input) {
+  var recursion = function (obj) {
+    if (obj.value > input) {
+      if (obj.left) {
+        recursion(obj.left)
+      } else {
+        obj.left = { value: input }
+      }
+    } else {
+      if (obj.right) {
+        recursion(obj.right)
+      } else {
+        obj.right = { value: input }
+      }
+    }
+  }
+  recursion(this)
+
+  console.log(this)
+}
+
+// accepts a value and returns a boolean reflecting whether or not the value is contained in the tree
+binaryMethods.contains = function (target) {
+  var recursion = function (obj) {
+    if (obj.value === target) {
+      return true
+    } else if (obj.value > target) {
+      if (obj.left) {
+        return recursion(obj.left)
+      }
+    } else {
+      if (obj.right) {
+        return recursion(obj.right)
+      }
+    }
+    return false
+  }
+
+  return recursion(this)
+}
+
+// accepts a callback and executes it on every value contained in the tree
+binaryMethods.depthFirstLog = function (callback) {
+  var recursion = function (obj) {
+    if (obj.value) {
+      callback(obj.value)
+    }
+    if (obj.left) {
+      recursion(obj.left)
+    }
+    if (obj.right) {
+      recursion(obj.right)
+    }
+  }
+  recursion(this)
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-if ( typeof module === "object" && typeof module.exports === "object" ) {
-  module.exports = BinarySearchTree;
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = BinarySearchTree
 }
