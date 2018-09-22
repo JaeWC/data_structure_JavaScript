886d2273f8e2b081687fffac1dea46efd861973f
Jae Choi <j.thechois@gmail.com>
2018-09-22 17:34:25 +0900

    hasTable Advanced Complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,6 +4,7 @@ if (typeof module === 'object' && typeof module.exports === 'object') {
 
 var HashTable = function () {
   this._limit = 8
+  this._size = 0
   this._storage = LimitedArray(this._limit)
 }
 
@@ -13,6 +14,11 @@ HashTable.prototype.insert = function (k, v) {
   var innerBuckets = []
   var innerTuples = {}
   innerTuples[k] = v
+  this._size++
+  if (this._size > this._limit * 0.75) {
+    this.resize(this._limit * 2)
+    index = getIndexBelowMaxForKey(k, this._limit)
+  }
   if (this._storage.get(index) === undefined) {
     innerBuckets.push(innerTuples)
     this._storage.set(index, innerBuckets)
@@ -42,11 +48,35 @@ HashTable.prototype.retrieve = function (k) {
 HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit)
 
+  this._size--
+  if (this._size < this._limit * 0.25) {
+    this.resize(this._limit / 2)
+    index = getIndexBelowMaxForKey(k, this._limit)
+  }
+
   for (var i = 0; i < this._storage.get(index).length; i++) {
     if (this._storage.get(index)[i][k]) {
       delete this._storage.get(index)[i]
     }
   }
+}
+
+HashTable.prototype.resize = function (newLimit) {
+  this._limit = newLimit
+  var oldStorage = this._storage
+  this._storage = LimitedArray(this._limit)
+
+  oldStorage.each((pairs) => {
+    if (pairs) {
+      for (var i = 0; i < pairs.length; i++) {
+        if (pairs[i]) {
+          var pair = pairs[i]
+          this.insert(Object.keys(pair)[0], Object.values(pair)[0])
+          this._size--
+        }
+      }
+    }
+  })
 }
 /*
  * Complexity: What is the time complexity of the above functions?
